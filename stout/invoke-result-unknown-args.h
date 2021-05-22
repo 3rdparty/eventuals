b996c15b68778def6913785416eb6dfc131b8f44
Benjamin Hindman <benjamin.hindman@gmail.com>
2021-05-21 20:03:44 -0700

    Improved experience with InvokeResultUnknownArgs.

--- stout/invoke-result-unknown-args.h
+++ stout/invoke-result-unknown-args.h
@@ -22,10 +22,17 @@ auto operator+(AnyArg arg, T&& t)
 // TODO(benh): add more overloads for operators (-, !, etc).
 
 
-template <typename F, typename... Args>
+template <typename F, size_t n = 16, typename... Args>
 struct InvokeResultUnknownArgs : std::conditional_t<
   std::is_invocable_v<F, Args...>,
   std::invoke_result<F, Args...>,
-  InvokeResultUnknownArgs<F, Args..., AnyArg>> {};
+  InvokeResultUnknownArgs<F, n - 1, Args..., AnyArg>>
+{
+  static_assert(
+      n != 0,
+      "tried up to 16 arguments; either make your "
+      "argument types more explicit (i.e, don't use"
+      " 'auto') or specify a larger 'n'");
+};
 
 } // namespace stout {
