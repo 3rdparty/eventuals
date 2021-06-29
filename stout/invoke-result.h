73412e9c132405cddff6e660839c9f48849d69d6
Benjamin Hindman <benjamin.hindman@gmail.com>
2021-06-28 19:47:32 -0700

    Removed InvokeResultUnknownArgs because it doesn't work with 'auto'.

--- stout/invoke-result.h
+++ stout/invoke-result.h
@@ -19,60 +19,6 @@ struct type_identity
 
 ////////////////////////////////////////////////////////////////////////
 
-struct AnyArg
-{
-  template <typename T>
-  operator T&& () { throw std::runtime_error("unexpected invocation"); }
-
-  template <typename T>
-  operator T& () { throw std::runtime_error("unexpected invocation"); }
-};
-
-
-template <typename T>
-auto operator+(AnyArg arg, T&& t)
-{
-  return static_cast<T>(arg) + std::forward<T>(t);
-}
-
-// TODO(benh): add more overloads for operators (-, !, etc).
-
-////////////////////////////////////////////////////////////////////////
-
-namespace detail {
-
-////////////////////////////////////////////////////////////////////////
-
-template <size_t n>
-constexpr size_t decrement()
-{
-  static_assert(
-      n > 1,
-      "could not determine invoke result; either make "
-      "your argument types more explicit (i.e, don't use"
-      " 'auto') or specify a larger 'n'");
-
-  return n - 1;
-}
-
-////////////////////////////////////////////////////////////////////////
-
-} // namespace detail {
-
-////////////////////////////////////////////////////////////////////////
-
-template <typename F, size_t n = 12, typename... Args>
-struct InvokeResultUnknownArgs : std::conditional_t<
-  std::is_invocable_v<F, Args...>,
-  std::invoke_result<F, Args...>,
-  InvokeResultUnknownArgs<F, detail::decrement<n>(), Args..., AnyArg>> {};
-
-
-template <typename F, typename... Args>
-struct InvokeResultUnknownArgs<F, 0, Args...> {};
-
-////////////////////////////////////////////////////////////////////////
-
 template <typename F, typename... Values>
 struct InvokeResultPossiblyUndefined
 {
