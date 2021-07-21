d97e8992722ac93f83a8deec727cbb65af8a7e62
Benjamin Hindman <benjamin.hindman@gmail.com>
2021-07-21 08:47:23 -0700

    Improvements and bug fixes for 'Closure'.

--- stout/closure.h
+++ stout/closure.h
@@ -16,21 +16,19 @@ namespace detail {
 
 ////////////////////////////////////////////////////////////////////////
 
-template <typename K_, typename F_>
+template <typename K_, typename F_, typename Arg_>
 struct Closure
 {
-  using E_ = typename InvokeResultPossiblyUndefined<F_>::type;
+  using E_ = decltype(eventuals::compose<Arg_>(std::declval<F_>()()));
 
-  using Value = typename ValueFrom<
-    K_,
-    typename ValuePossiblyUndefined<E_>::Value>::type;
+  using Value = typename ValueFrom<K_, typename E_::Value>::type;
 
   Closure(K_ k, F_ f) : k_(std::move(k)), f_(std::move(f)) {}
 
-  template <typename K, typename F>
+  template <typename Arg, typename K, typename F>
   static auto create(K k, F f)
   {
-    return Closure<K, F>(std::move(k), std::move(f));
+    return Closure<K, F, Arg>(std::move(k), std::move(f));
   }
 
   template <
@@ -39,7 +37,7 @@ struct Closure
       IsContinuation<K>::value, int> = 0>
   auto k(K k) &&
   {
-    return create(
+    return create<Arg_>(
         [&]() {
           if constexpr (!IsUndefined<K_>::value) {
             return std::move(k_) | std::move(k);
@@ -56,60 +54,72 @@ struct Closure
       !IsContinuation<F>::value, int> = 0>
   auto k(F f) &&
   {
-    return std::move(*this) | eventuals::Lambda(std::move(f));
+    auto lambda = []() {
+      return std::declval<E_>() | eventuals::Lambda(std::declval<F>());
+    };
+
+    auto map = []() {
+      return std::declval<E_>() | eventuals::Map(std::declval<F>());
+    };
+
+    static_assert(
+        !std::is_invocable_v<decltype(lambda)>
+        || std::is_invocable_v<decltype(map)>,
+        "'Closure' can't determine how to compose non-continuation, "
+        "couldn't use either 'Lambda' or 'Map'");
+
+    if constexpr (std::is_invocable_v<decltype(lambda)>) {
+      return std::move(*this) | eventuals::Lambda(std::move(f));
+    } else {
+      return std::move(*this) | eventuals::Map(std::move(f));
+    }
   }
 
   template <typename... Args>
   void Start(Args&&... args)
   {
-    if (!adaptor_) {
-      adaptor_.emplace(
-          f_()
-          | Adaptor<K_, typename E_::Value>(
-              k_,
-              [](auto& k_, auto&&... values) {
-                eventuals::succeed(k_, std::forward<decltype(values)>(values)...);
-              }));
-
-      if (interrupt_ != nullptr) {
-        adaptor_->Register(*interrupt_);
-      }
-    }
-
-    eventuals::succeed(*adaptor_, std::forward<Args>(args)...);
+    eventuals::succeed(continuation(), std::forward<Args>(args)...);
   }
 
   template <typename... Args>
   void Fail(Args&&... args)
   {
-    eventuals::fail(k_, std::forward<Args>(args)...);
+    eventuals::fail(continuation(), std::forward<Args>(args)...);
   }
 
   void Stop()
   {
-    eventuals::stop(k_);
+    eventuals::stop(continuation());
   }
 
   template <typename... Args>
   void Body(Args&&... args)
   {
-    assert(adaptor_);
-
-    eventuals::body(*adaptor_, std::forward<Args>(args)...);
+    eventuals::body(continuation(), std::forward<Args>(args)...);
   }
 
   void Ended()
   {
-    assert(adaptor_);
-
-    eventuals::ended(*adaptor_);
+    eventuals::ended(continuation());
   }
 
   void Register(Interrupt& interrupt)
   {
     assert(interrupt_ == nullptr);
     interrupt_ = &interrupt;
-    k_.Register(interrupt);
+  }
+
+  auto& continuation()
+  {
+    if (!continuation_) {
+      continuation_.emplace(eventuals::compose<Arg_>(f_()) | std::move(k_));
+
+      if (interrupt_ != nullptr) {
+        continuation_->Register(*interrupt_);
+      }
+    }
+
+    return *continuation_;
   }
 
   K_ k_;
@@ -117,11 +127,9 @@ struct Closure
 
   Interrupt* interrupt_ = nullptr;
 
-  using Adaptor_ = typename EKPossiblyUndefined<
-    E_,
-    Adaptor<K_, typename ValuePossiblyUndefined<E_>::Value>>::type;
+  using Continuation_ = typename EKPossiblyUndefined<E_, K_>::type;
 
-  std::optional<Adaptor_> adaptor_;
+  std::optional<Continuation_> continuation_;
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -130,38 +138,35 @@ struct Closure
 
 ////////////////////////////////////////////////////////////////////////
 
-template <typename K, typename F>
+template <typename K, typename F, typename Arg>
 struct IsContinuation<
-  detail::Closure<K, F>> : std::true_type {};
+  detail::Closure<K, F, Arg>> : std::true_type {};
 
 ////////////////////////////////////////////////////////////////////////
 
-template <typename K, typename F>
+template <typename K, typename F, typename Arg>
 struct HasTerminal<
-  detail::Closure<K, F>> : HasTerminal<K> {};
+  detail::Closure<K, F, Arg>> : HasTerminal<K> {};
 
 ////////////////////////////////////////////////////////////////////////
 
-template <typename K, typename F>
-struct Compose<detail::Closure<K, F>>
+template <typename K, typename F, typename Arg_>
+struct Compose<detail::Closure<K, F, Arg_>>
 {
   template <typename Arg>
-  static auto compose(detail::Closure<K, F> closure)
+  static auto compose(detail::Closure<K, F, Arg_> closure)
   {
     if constexpr (!IsUndefined<Arg>::value) {
+      // NOTE: we do a 'compose()' with the closure's callable in
+      // order to propagate type information to the callable.
       auto f = eventuals::compose<Arg>(std::move(closure.f_));
 
       using E = decltype(f());
 
-      static_assert(
-          IsContinuation<E>::value,
-          "expecting eventual continuation as "
-          "result of callable passed to 'Closure'");
-
       using Value = typename E::Value;
 
       auto k = eventuals::compose<Value>(std::move(closure.k_));
-      return detail::Closure<decltype(k), decltype(f)>(
+      return detail::Closure<decltype(k), decltype(f), Arg>(
           std::move(k),
           std::move(f));
     } else {
@@ -175,7 +180,19 @@ struct Compose<detail::Closure<K, F>>
 template <typename F>
 auto Closure(F f)
 {
-  return detail::Closure<Undefined, F>(Undefined(), std::move(f));
+  static_assert(
+      std::is_invocable_v<F>,
+      "'Closure' expects a *callable* (e.g., a lambda or functor) "
+      "that doesn't expect any arguments");
+
+  using E = decltype(f());
+
+  static_assert(
+      IsContinuation<E>::value,
+      "Expecting an eventual continuation as the "
+      "result of the callable passed to 'Closure'");
+
+  return detail::Closure<Undefined, F, Undefined>(Undefined(), std::move(f));
 }
 
 ////////////////////////////////////////////////////////////////////////
