c2dab2dee93d9be7be715216c61fda1ac03a5803
Artur Bandaryk <arthur_bandaryk@mail.ru>
2021-08-19 12:43:08 +0300

    Solo function for dns resolver (ip::Resolve()]

--- stout/libuv/dns-resolver.h
+++ stout/libuv/dns-resolver.h
@@ -8,85 +8,81 @@
 
 namespace stout {
 namespace uv {
+namespace ip {
 
 using stout::uv::Loop;
 
-class DomainNameResolver {
- public:
-  DomainNameResolver() {
-    p_hints = std::make_unique<addrinfo>();
-    p_hints->ai_family = PF_INET;
-    p_hints->ai_socktype = SOCK_STREAM;
-    p_hints->ai_protocol = IPPROTO_TCP;
-    p_hints->ai_flags = 0;
-    resolver = std::make_unique<uv_getaddrinfo_t>();
-  }
+auto Resolve(
+    Loop& loop,
+    const std::string& address,
+    const std::string& port) {
+  struct Data {
+    addrinfo hints;
+    uv_getaddrinfo_t resolver;
+    Data() {
+      hints.ai_family = PF_INET;
+      hints.ai_socktype = SOCK_STREAM;
+      hints.ai_protocol = IPPROTO_TCP;
+      hints.ai_flags = 0;
+    }
+  };
 
-  // we overload () in order to have the possibility
-  // to compose this object with other async/sync tasks
-  auto operator()(
-      Loop& loop,
-      const std::string& address,
-      const std::string& port) {
-    return stout::eventuals::Eventual<std::string>()
-        .start([&](auto& k) {
-          resolver->data = &k; // we should store our continuation k
-          // in the field data(void*), because we can't use
-          // capturing lambdas as callbacks for libuv!
-          // the link down will be helpfull:
-          // https://misfra.me/2016/02/24/libuv-and-cpp/
+  return stout::eventuals::Eventual<std::string>()
+      .context(Data())
+      .start([&](auto& data, auto& k) {
+        data.resolver.data = &k; // we should store our continuation k
+        // in the field data(void*), because we can't use
+        // capturing lambdas as callbacks for libuv!
+        // the link down will be helpfull:
+        // https://misfra.me/2016/02/24/libuv-and-cpp/
 
 
-          //callback
-          auto p_get_addr_info_cb = [](uv_getaddrinfo_t* req,
-                                       int status,
-                                       struct addrinfo* res) {
-            if (status < 0) {
+        //callback
+        auto p_get_addr_info_cb = [](uv_getaddrinfo_t* req,
+                                     int status,
+                                     struct addrinfo* res) {
+          if (status < 0) {
+            stout::eventuals::fail(
+                *static_cast<decltype(&k)>(req->data),
+                std::string{uv_err_name(status)});
+          } else {
+            // addr array is for resulting ip from the specific node
+            char addr[17] = {'\0'};
+
+            // get to addr the resulting ip
+            auto result = uv_ip4_name(
+                (struct sockaddr_in*) res->ai_addr,
+                addr,
+                16);
+
+            // if there was an error we just fail our continuation
+            if (result) {
               stout::eventuals::fail(
                   *static_cast<decltype(&k)>(req->data),
-                  std::string{uv_err_name(status)});
+                  std::string{uv_err_name(result)});
             } else {
-              // addr array is for resulting ip from the specific node
-              char addr[17] = {'\0'};
-
-              // get to addr the resulting ip
-              auto result = uv_ip4_name(
-                  (struct sockaddr_in*) res->ai_addr,
-                  addr,
-                  16);
-
-              // if there was an error we just fail our continuation
-              if (result) {
-                stout::eventuals::fail(
-                    *static_cast<decltype(&k)>(req->data),
-                    std::string{uv_err_name(result)});
-              } else {
-                // succeed the resulting ip
-                stout::eventuals::succeed(
-                    *static_cast<decltype(&k)>(req->data),
-                    std::string{addr});
-              }
+              // succeed the resulting ip
+              stout::eventuals::succeed(
+                  *static_cast<decltype(&k)>(req->data),
+                  std::string{addr});
             }
-          };
+          }
+        };
 
-          auto result = uv_getaddrinfo(
-              loop,
-              resolver.get(),
-              p_get_addr_info_cb,
-              address.c_str(),
-              port.c_str(),
-              p_hints.get());
+        auto result = uv_getaddrinfo(
+            loop,
+            &(data.resolver),
+            p_get_addr_info_cb,
+            address.c_str(),
+            port.c_str(),
+            &(data.hints));
 
-          // if there was an error we just fail our continuation
-          if (result)
-            stout::eventuals::fail(k, std::string{uv_err_name(result)});
-        });
-  }
+        // if there was an error we just fail our continuation
+        if (result)
+          stout::eventuals::fail(k, std::string{uv_err_name(result)});
+      });
+}
 
- private:
-  std::unique_ptr<addrinfo> p_hints;
-  std::unique_ptr<uv_getaddrinfo_t> resolver;
-};
-
+} // namespace ip
 } // namespace uv
 } // namespace stout
