80c7de73e188f31bdafcfb35f01281b76ccdf1ba
Zakhar Pilipuk <zpilipuk@gmail.com>
2021-09-15 14:29:09 +0300

    Add Collect wrapper

---
+++ test/collect.cc
@@ -0,0 +1,43 @@
+#include "stout/collect.h"
+
+#include <set>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "stout/iterate.h"
+#include "stout/terminal.h"
+
+namespace eventuals = stout::eventuals;
+
+using stout::eventuals::Collect;
+using stout::eventuals::Iterate;
+
+TEST(Collect, CommonVectorPass) {
+  std::vector<int> v = {5, 12};
+
+  auto s = [&]() {
+    return Iterate(v)
+        | Collect<std::vector<int>>();
+  };
+
+  std::vector<int> res = *s();
+
+  EXPECT_EQ(5, res.at(0));
+  EXPECT_EQ(12, res.at(1));
+  EXPECT_EQ(2, res.size());
+}
+
+TEST(Collect, CommonSetPass) {
+  std::set<int> v = {5, 12};
+
+  auto s = [&]() {
+    return Iterate(v)
+        | Collect<std::set<int>>();
+  };
+
+  std::set<int> res = *s();
+
+  ASSERT_TRUE(res.size() == 2);
+  EXPECT_EQ(5, *res.begin());
+  EXPECT_EQ(12, *++res.begin());
+}
