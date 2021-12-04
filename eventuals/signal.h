b1c4761d826e656f95b265eb27122e0511fc0c5a
Benjamin Hindman <benjamin.hindman@gmail.com>
2021-12-03 16:56:37 -1000

    Introduce 'WaitForSignal()'
    The current helpers for signal handling are relatively low-level as
    they were designed to wrap the low-level 'uv_signal*()' family of
    functions. In practice we often want to be able to specify multiple
    signals and find out which signal was raised; this commit introduces
    the helper 'WaitForSignal()' which does just that.

--- eventuals/signal.h
+++ eventuals/signal.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "eventuals/concurrent.h"
 #include "eventuals/event-loop.h"
+#include "eventuals/head.h"
+#include "eventuals/iterate.h"
+#include "eventuals/just.h"
+#include "eventuals/map.h"
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -16,6 +21,30 @@ inline auto Signal(EventLoop& loop, const int& signum) {
 
 inline auto Signal(const int& signum) {
   return EventLoop::Default().Signal(signum);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+// Eventual that waits for one of the specified signals to be raised
+// and then propagates the raised signal number to the next eventual.
+//
+// Note that all standard signal handling constraints still apply,
+// i.e., you can't have more than one handler for the same signal,
+// which in this case means you can't have more than one of
+// outstanding calls to this function with the same signal.
+//
+// NOTE: we take an array instead of a 'std::initializer_list' because
+// then we can 'std::move()' into 'Iterate()' without a copy.
+template <typename T, size_t n>
+auto WaitForSignal(T(&&signums)[n]) {
+  return Iterate(std::move(signums))
+      | Concurrent([]() {
+           return Map([](int signum) {
+             return Signal(signum)
+                 | Just(signum);
+           });
+         })
+      | Head();
 }
 
 ////////////////////////////////////////////////////////////////////////
--- test/signal.cc
+++ test/signal.cc
@@ -15,6 +15,7 @@ using eventuals::Interrupt;
 using eventuals::Signal;
 using eventuals::Terminate;
 using eventuals::Then;
+using eventuals::WaitForSignal;
 
 using namespace std::chrono_literals;
 
@@ -57,6 +58,31 @@ TEST_F(SignalTest, SignalComposition) {
   EventLoop::Default().Run();
 
   EXPECT_EQ(future.get(), "quit");
+}
+
+TEST_F(SignalTest, WaitForSignal) {
+  auto e = WaitForSignal({SIGQUIT});
+
+  auto [future, k] = Terminate(std::move(e));
+
+  k.Start();
+
+  // NOTE: now that we've started the continuation 'k' we will have
+  // submitted a callback to the event loop and thus by explicitly
+  // submitting another callback we will ensure there is a
+  // happens-before relationship between setting up the signal handler
+  // and raising the signal.
+  EventLoop::Waiter waiter(&EventLoop::Default(), "raise(SIGQUIT)");
+
+  EventLoop::Default().Submit(
+      []() {
+        EXPECT_EQ(raise(SIGQUIT), 0);
+      },
+      &waiter);
+
+  EventLoop::Default().Run();
+
+  EXPECT_EQ(future.get(), SIGQUIT);
 }
 
 #endif // !defined(_WIN32)
