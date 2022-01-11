e9acbb3f8a352ce904243097d2742100732da826
Benjamin Hindman <benjamin.hindman@gmail.com>
2022-01-10 23:34:05 -0800

    Add HTTP mock server for testing

---
+++ test/http-mock-server.h
@@ -0,0 +1,282 @@
+#include "asio.hpp"
+#include "asio/ssl.hpp"
+#include "test/pem.h"
+#include "test/rsa.h"
+#include "test/x509.h"
+
+////////////////////////////////////////////////////////////////////////
+
+// Provides an HTTP mock server. See existing tests for examples of
+// how you can use 'EXPECT_CALL()' on methods like 'ReceivedHeaders()'
+// in your tests to handle each accepted socket.
+//
+// Only one socket is accepted and handled at a time.
+//
+// NOTE: this class is only expected to be used in tests so it
+// generously uses macros like 'EXPECT_*' and 'ADD_FAILURE'.
+class HttpMockServer {
+ public:
+  // Abstracts whether or not we have a secure, i.e., TLS/SSL, socket
+  // or an insecure socket.
+  class Socket {
+   public:
+    virtual ~Socket() {}
+    virtual std::string Receive() = 0;
+    virtual void Send(const std::string& data) = 0;
+    virtual void Close() = 0;
+  };
+
+  // Implementation of an insecure socket, i.e, no TLS/SSL.
+  class InsecureSocket : public Socket {
+   public:
+    InsecureSocket(asio::ip::tcp::socket socket)
+      : socket_(std::move(socket)) {}
+
+    std::string Receive() override {
+      asio::error_code error;
+      char data[4096];
+      size_t bytes = socket_.receive(
+          asio::buffer(data, 4096),
+          /* flags = */ 0,
+          error);
+      if (error) {
+        ADD_FAILURE() << "Failed to receive: " << error.message();
+        return std::string();
+      } else {
+        return std::string(data, bytes);
+      }
+    }
+
+    void Send(const std::string& data) override {
+      size_t bytes = socket_.send(asio::buffer(data.data(), data.size()));
+      EXPECT_EQ(bytes, data.size());
+    }
+
+    void Close() override {
+      asio::error_code error;
+      socket_.close(error);
+      if (error) {
+        ADD_FAILURE() << "Failed to close the socket: " << error.message();
+      }
+    }
+
+   private:
+    asio::ip::tcp::socket socket_;
+  };
+
+  // Implementation of an secure socket.
+  class SecureSocket : public Socket {
+   public:
+    SecureSocket(asio::ssl::stream<asio::ip::tcp::socket> stream)
+      : stream_(std::move(stream)) {}
+
+    std::string Receive() override {
+      asio::error_code error;
+      char data[1024];
+      size_t bytes = stream_.read_some(asio::buffer(data, 1024), error);
+      if (error) {
+        ADD_FAILURE() << "Failed to receive: " << error.message();
+        return std::string();
+      } else {
+        return std::string(data, bytes);
+      }
+    }
+
+    void Send(const std::string& data) override {
+      size_t bytes = stream_.write_some(asio::buffer(data.data(), data.size()));
+      EXPECT_EQ(bytes, data.size());
+    }
+
+    void Close() override {
+      asio::error_code error;
+      stream_.next_layer().close(error);
+      if (error) {
+        ADD_FAILURE() << "Failed to close the socket: " << error.message();
+      }
+    }
+
+   private:
+    asio::ssl::stream<asio::ip::tcp::socket> stream_;
+  };
+
+  HttpMockServer(const std::string& scheme)
+    : scheme_(scheme),
+      acceptor_(io_context_),
+      ssl_context_(asio::ssl::context::sslv23) {
+    // Ensure we can first open, bind, listen an "acceptor".
+    asio::error_code error;
+
+    acceptor_.open(asio::ip::tcp::v4(), error);
+
+    EXPECT_FALSE(error) << error.message();
+
+    acceptor_.bind(
+        asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 0),
+        error);
+
+    EXPECT_FALSE(error) << error.message();
+
+    acceptor_.listen(asio::socket_base::max_listen_connections, error);
+
+    EXPECT_FALSE(error) << error.message();
+
+    endpoint_ = acceptor_.local_endpoint();
+
+    EXPECT_FALSE(error) << error.message();
+
+    // Now configure our SSL context with a newly generated X509
+    // certificate that we self-sign with a newly generated RSA
+    // private key.
+    auto key = rsa::Builder().Build();
+
+    CHECK(key) << "Failed to generate RSA private key";
+
+    auto pem_key = pem::Encode(*key);
+
+    CHECK(pem_key) << "Failed to PEM encode RSA private key";
+
+    ssl_context_.use_private_key(
+        asio::buffer(*pem_key),
+        asio::ssl::context::pem);
+
+    auto certificate = x509::Builder()
+                           .subject_key(*key)
+                           .sign_key(*key)
+                           .ip(endpoint_.address())
+                           .Build();
+
+    CHECK(certificate) << "Failed to generate X509 certificate";
+
+    auto pem_certificate = pem::Encode(*certificate);
+
+    CHECK(pem_certificate) << "Failed to PEM encode X509 certificate";
+
+    ssl_context_.use_certificate_chain(
+        asio::buffer(*pem_certificate));
+
+    // Now set up what our mock functions will do by default if the
+    // test using this class doesn't do 'EXPECT_CALL()'.
+    //
+    // All of these callbacks are made by a new thread that we create
+    // hence the use of blocking functions (and only one socket is
+    // accepted and handled at a time).
+    //
+    // NOTE: using 'EXPECT_CALL().WillRepeatedly()' here instead of
+    // 'ON_CALL().WillByDefault()' to suppress "Uninteresting mock
+    // function" messages as suggested by the gmock cookbook.
+    EXPECT_CALL(*this, Accepted)
+        .WillRepeatedly(
+            [this](std::unique_ptr<Socket> socket) {
+              // Receive data up to end of headers.
+              std::string data;
+
+              do {
+                std::string buffer = socket->Receive();
+                if (buffer.empty()) {
+                  socket->Close();
+                  return;
+                }
+                data += buffer;
+              } while (data.find("\r\n\r\n") == std::string::npos);
+
+              ReceivedHeaders(std::move(socket), data);
+            });
+
+    // NOTE: using 'EXPECT_CALL().WillRepeatedly()' here instead of
+    // 'ON_CALL().WillByDefault()' to suppress "Uninteresting mock
+    // function" messages as suggested by the gmock cookbook.
+    EXPECT_CALL(*this, ReceivedHeaders)
+        .WillRepeatedly(
+            [](std::unique_ptr<Socket> socket, const std::string& data) {
+              socket->Close();
+            });
+
+    // Now create the thread for accepting and handlings sockets,
+    // which also appropriately handles whether or not to expect
+    // secure ('https://') or insecure ('http://') clients.
+    thread_ = std::thread([this]() {
+      asio::error_code error;
+      do {
+        asio::ip::tcp::socket socket = acceptor_.accept(error);
+        if (!error) {
+          if (scheme_ == "http://") {
+            Accepted(std::unique_ptr<Socket>(
+                new InsecureSocket(std::move(socket))));
+          } else {
+            CHECK_EQ(scheme_, "https://");
+
+            asio::ssl::stream<asio::ip::tcp::socket> stream(
+                std::move(socket),
+                ssl_context_);
+
+            stream.set_verify_mode(asio::ssl::verify_none);
+            stream.handshake(asio::ssl::stream_base::server, error);
+
+            if (error) {
+              ADD_FAILURE()
+                  << "Failed to perform TLS/SSL handshake: " << error.message();
+            } else {
+              Accepted(std::unique_ptr<Socket>(
+                  new SecureSocket(std::move(stream))));
+            }
+          }
+        }
+      } while (!error);
+    });
+  }
+
+  ~HttpMockServer() {
+    // Close the acceptor to force 'accept()' to error out and the
+    // thread to exit and be joinable.
+    asio::error_code error;
+    acceptor_.close(error);
+    EXPECT_FALSE(error) << error.message();
+    thread_.join();
+  }
+
+  // Mock method that allows overloading in each test via an
+  // 'EXPECT_CALL()' how to handle a newly accepted socket.
+  MOCK_METHOD(
+      void,
+      Accepted,
+      (std::unique_ptr<Socket>),
+      (const));
+
+  // Mock method that allows overloading in each test via an
+  // 'EXPECT_CALL()' what to do with a socket after the headers have
+  // been received.
+  MOCK_METHOD(
+      void,
+      ReceivedHeaders,
+      (std::unique_ptr<Socket>, const std::string&),
+      (const));
+
+  // TODO(benh): consider a 'ReceivedBody()' mock function but it's a
+  // bit trickier since the body might be "chunked".
+
+  unsigned short port() const {
+    return endpoint_.port();
+  }
+
+  std::string host() const {
+    return endpoint_.address().to_string();
+  }
+
+  std::string authority() const {
+    return host() + ":" + std::to_string(port());
+  }
+
+  std::string uri() const {
+    return scheme_ + authority();
+  }
+
+ private:
+  std::string scheme_;
+  asio::io_context io_context_;
+  asio::ip::tcp::endpoint endpoint_;
+  asio::ip::tcp::acceptor acceptor_;
+  asio::ssl::context ssl_context_;
+  std::thread thread_;
+};
+
+////////////////////////////////////////////////////////////////////////
---
+++ test/x509.h
@@ -0,0 +1,290 @@
+#include "eventuals/expected.h"
+#include "openssl/x509.h"
+
+////////////////////////////////////////////////////////////////////////
+
+namespace x509 {
+
+////////////////////////////////////////////////////////////////////////
+
+// Builder for generating an X509 certificate.
+class Builder {
+ public:
+  Builder() {}
+
+  eventuals::Expected::Of<X509*> Build() &&;
+
+  Builder subject_key(EVP_PKEY* key) && {
+    subject_key_ = key;
+    return std::move(*this);
+  }
+
+  Builder sign_key(EVP_PKEY* key) && {
+    sign_key_ = key;
+    return std::move(*this);
+  }
+
+  Builder parent_certificate(X509* certificate) && {
+    parent_certificate_ = certificate;
+    return std::move(*this);
+  }
+
+  Builder serial(int serial) && {
+    serial_ = serial;
+    return std::move(*this);
+  }
+
+  Builder days(int days) && {
+    days_ = days;
+    return std::move(*this);
+  }
+
+  Builder hostname(std::string&& hostname) && {
+    hostname_ = std::move(hostname);
+    return std::move(*this);
+  }
+
+  Builder ip(asio::ip::address&& ip) && {
+    ip_ = std::move(ip);
+    return std::move(*this);
+  }
+
+ private:
+  EVP_PKEY* subject_key_;
+  EVP_PKEY* sign_key_;
+  std::optional<X509*> parent_certificate_;
+  int serial_ = 1;
+  int days_ = 365;
+  std::optional<std::string> hostname_;
+  std::optional<asio::ip::address> ip_;
+};
+
+////////////////////////////////////////////////////////////////////////
+
+eventuals::Expected::Of<X509*> Builder::Build() && {
+  // Using a 'using' here to reduce verbosity.
+  using eventuals::Unexpected;
+
+  std::optional<X509_NAME*> issuer_name;
+
+  if (!parent_certificate_.has_value()) {
+    // If there is no parent certificate, then the subject and
+    // signing key must be the same.
+    if (subject_key_ != sign_key_) {
+      return Unexpected("Subject vs signing key mismatch");
+    }
+  } else {
+    // If there is a parent certificate, then set the issuer name to
+    // be that of the parent.
+    issuer_name = X509_get_subject_name(parent_certificate_.value());
+
+    if (issuer_name.value() == nullptr) {
+      return Unexpected(
+          "Failed to get subject name of parent certificate: "
+          "X509_get_subject_name");
+    }
+  }
+
+  // Allocate the in-memory structure for the certificate.
+  X509* x509 = X509_new();
+  if (x509 == nullptr) {
+    return Unexpected("Failed to allocate certification: X509_new");
+  }
+
+  // Set the version to V3.
+  if (X509_set_version(x509, 2) != 1) {
+    X509_free(x509);
+    return Unexpected("Failed to set version: X509_set_version");
+  }
+
+  // Set the serial number.
+  if (ASN1_INTEGER_set(X509_get_serialNumber(x509), serial_) != 1) {
+    X509_free(x509);
+    return Unexpected("Failed to set serial number: ASN1_INTEGER_set");
+  }
+
+  // Make this certificate valid for 'days' number of days from now.
+  if (X509_gmtime_adj(
+          X509_get_notBefore(x509),
+          0)
+          == nullptr
+      || X509_gmtime_adj(
+             X509_get_notAfter(x509),
+             60L * 60L * 24L * days_)
+          == nullptr) {
+    X509_free(x509);
+    return Unexpected(
+        "Failed to set valid days of certificate: X509_gmtime_adj");
+  }
+
+  // Set the public key for our certificate based on the subject key.
+  if (X509_set_pubkey(x509, subject_key_) != 1) {
+    X509_free(x509);
+    return Unexpected("Failed to set public key: X509_set_pubkey");
+  }
+
+  // Figure out our hostname if one was not provided.
+  if (!hostname_.has_value()) {
+    asio::error_code error;
+    hostname_ = asio::ip::host_name(error);
+    if (error) {
+      X509_free(x509);
+      return Unexpected("Failed to determine hostname");
+    }
+  }
+
+  // Grab the subject name of the new certificate.
+  X509_NAME* name = X509_get_subject_name(x509);
+  if (name == nullptr) {
+    X509_free(x509);
+    return Unexpected("Failed to get subject name: X509_get_subject_name");
+  }
+
+  // Set the country code, organization, and common name.
+  if (X509_NAME_add_entry_by_txt(
+          name,
+          "C",
+          MBSTRING_ASC,
+          reinterpret_cast<const unsigned char*>("US"),
+          -1,
+          -1,
+          0)
+      != 1) {
+    X509_free(x509);
+    return Unexpected("Failed to set country code: X509_NAME_add_entry_by_txt");
+  }
+
+  if (X509_NAME_add_entry_by_txt(
+          name,
+          "O",
+          MBSTRING_ASC,
+          reinterpret_cast<const unsigned char*>("Test"),
+          -1,
+          -1,
+          0)
+      != 1) {
+    X509_free(x509);
+    return Unexpected(
+        "Failed to set organization name: X509_NAME_add_entry_by_txt");
+  }
+
+  if (X509_NAME_add_entry_by_txt(
+          name,
+          "CN",
+          MBSTRING_ASC,
+          reinterpret_cast<const unsigned char*>(hostname_->c_str()),
+          -1,
+          -1,
+          0)
+      != 1) {
+    X509_free(x509);
+    return Unexpected("Failed to set common name: X509_NAME_add_entry_by_txt");
+  }
+
+  // Set the issuer name to be the same as the subject if it is not
+  // already set (this is a self-signed certificate).
+  if (!issuer_name.has_value()) {
+    issuer_name = name;
+  }
+
+  CHECK(issuer_name.has_value());
+  if (X509_set_issuer_name(x509, issuer_name.value()) != 1) {
+    X509_free(x509);
+    return Unexpected("Failed to set issuer name: X509_set_issuer_name");
+  }
+
+  if (ip_.has_value()) {
+    // Add an X509 extension with an IP for subject alternative name.
+
+    STACK_OF(GENERAL_NAME)* alt_name_stack = sk_GENERAL_NAME_new_null();
+    if (alt_name_stack == nullptr) {
+      X509_free(x509);
+      return Unexpected("Failed to create a stack: sk_GENERAL_NAME_new_null");
+    }
+
+    GENERAL_NAME* alt_name = GENERAL_NAME_new();
+    if (alt_name == nullptr) {
+      sk_GENERAL_NAME_pop_free(alt_name_stack, GENERAL_NAME_free);
+      X509_free(x509);
+      return Unexpected("Failed to create GENERAL_NAME: GENERAL_NAME_new");
+    }
+
+    alt_name->type = GEN_IPADD;
+
+    ASN1_STRING* alt_name_str = ASN1_STRING_new();
+    if (alt_name_str == nullptr) {
+      GENERAL_NAME_free(alt_name);
+      sk_GENERAL_NAME_pop_free(alt_name_stack, GENERAL_NAME_free);
+      X509_free(x509);
+      return Unexpected("Failed to create alternative name: ASN1_STRING_new");
+    }
+
+    if (!ip_->is_v4()) {
+      return Unexpected("Only IPv4 is currently supported");
+    }
+
+    in_addr in;
+    in.s_addr = ip_->to_v4().to_ulong();
+
+#ifdef __WINDOWS__
+    // cURL defines `in_addr_t` as `unsigned long` for Windows,
+    // so we do too for consistency.
+    typedef unsigned long in_addr_t;
+#endif // __WINDOWS__
+
+    // For `iPAddress` we hand over a binary value as part of the
+    // specification.
+    if (ASN1_STRING_set(alt_name_str, &in.s_addr, sizeof(in_addr_t)) == 0) {
+      ASN1_STRING_free(alt_name_str);
+      GENERAL_NAME_free(alt_name);
+      sk_GENERAL_NAME_pop_free(alt_name_stack, GENERAL_NAME_free);
+      X509_free(x509);
+      return Unexpected("Failed to set alternative name: ASN1_STRING_set");
+    }
+
+    // We are transferring ownership of 'alt_name_str` towards the
+    // `ASN1_OCTET_STRING` here.
+    alt_name->d.iPAddress = alt_name_str;
+
+    // We try to transfer ownership of 'alt_name` towards the
+    // `STACK_OF(GENERAL_NAME)` here.
+    if (sk_GENERAL_NAME_push(alt_name_stack, alt_name) == 0) {
+      GENERAL_NAME_free(alt_name);
+      sk_GENERAL_NAME_pop_free(alt_name_stack, GENERAL_NAME_free);
+      X509_free(x509);
+      return Unexpected(
+          "Failed to push alternative name: sk_GENERAL_NAME_push");
+    }
+
+    // We try to transfer the ownership of `alt_name_stack` towards the
+    // `X509` here.
+    if (X509_add1_ext_i2d(
+            x509,
+            NID_subject_alt_name,
+            alt_name_stack,
+            0,
+            0)
+        == 0) {
+      sk_GENERAL_NAME_pop_free(alt_name_stack, GENERAL_NAME_free);
+      X509_free(x509);
+      return Unexpected(
+          "Failed to set subject alternative name: X509_add1_ext_i2d");
+    }
+
+    sk_GENERAL_NAME_pop_free(alt_name_stack, GENERAL_NAME_free);
+  }
+
+  // Sign the certificate with the sign key.
+  if (X509_sign(x509, sign_key_, EVP_sha1()) == 0) {
+    X509_free(x509);
+    return Unexpected("Failed to sign certificate: X509_sign");
+  }
+
+  return x509;
+}
+
+////////////////////////////////////////////////////////////////////////
+
+} // namespace x509
+
+////////////////////////////////////////////////////////////////////////
