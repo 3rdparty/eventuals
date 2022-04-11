d4429d7ad684edbdc20493f3456ff55f6aaf855e
Xander McCarthy <19922103+CodingCanuck@users.noreply.github.com>
2022-04-11 10:44:04 -1000

    Move Just() tests into a separate file (#318)
    Add tests for a void argument.

---
+++ test/just.cc
@@ -0,0 +1,67 @@
+#include "eventuals/just.h"
+
+#include <thread>
+
+#include "eventuals/just.h"
+#include "eventuals/terminal.h"
+#include "eventuals/then.h"
+#include "gtest/gtest.h"
+
+using eventuals::Just;
+using eventuals::Then;
+
+TEST(JustTest, Value) {
+  auto e = []() {
+    return Just(1);
+  };
+
+  EXPECT_EQ(1, *e());
+}
+
+TEST(JustTest, Void) {
+  bool ran = false;
+  auto e = [&]() {
+    return Just()
+        | Then([&]() {
+             ran = true;
+           });
+  };
+
+  ASSERT_FALSE(ran);
+  // A Just<void>() doesn't return anything.
+  *e();
+  ASSERT_TRUE(ran);
+}
+
+TEST(JustTest, Ref) {
+  int x = 10;
+
+  auto e = [&]() {
+    return Just(std::ref(x))
+        | Then([](int& x) {
+             x += 100;
+           });
+  };
+
+  *e();
+  EXPECT_EQ(110, x);
+}
+
+TEST(JustTest, ConstRef) {
+  int x = 10;
+
+  auto e = [&]() {
+    return Just(std::cref(x))
+        | Then([](const int& x) {
+             return std::cref(x);
+           });
+  };
+
+  auto [future, k] = Terminate(e());
+
+  k.Start();
+
+  x = 42;
+
+  EXPECT_EQ(42, future.get());
+}
