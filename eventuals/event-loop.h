bc7d84a9c5583e3733c05077278a7253290c0bf9
Nikita Nikolaenko <6870920@gmail.com>
2022-07-14 15:20:57 +0200

    Refactor #ifdef for better readability (#459)
    * Refactor #ifdef for better readability
    
    * Add a comment for why we need to use GetFileType

--- eventuals/event-loop.h
+++ eventuals/event-loop.h
@@ -794,15 +794,20 @@ class EventLoop final : public Scheduler {
               if (!completed_) {
                 CHECK(!started_);
                 started_ = true;
-#if !defined(_WIN32)
-                error_ = uv_poll_init(loop_, poll(), fd_);
-#else
+#ifdef _WIN32
+                // NOTE: FILE_TYPE_PIPE means that a handle is
+                // a socket, a named pipe, or an anonymous pipe.
+                // We need this piece of a code because libuv
+                // on Windows requires use of a separate function
+                // if the handle is of a socket type.
                 if (GetFileType(reinterpret_cast<HANDLE>(fd_))
                     == FILE_TYPE_PIPE) {
                   error_ = uv_poll_init_socket(loop_, poll(), fd_);
                 } else {
                   error_ = uv_poll_init(loop_, poll(), fd_);
                 }
+#else
+                error_ = uv_poll_init(loop_, poll(), fd_);
 #endif
                 if (!error_) {
                   uv_handle_set_data(handle(), this);
