b4b32e1d33cdfb148d0ba893312a18c156112987
Nikita Nikolaenko <6870920@gmail.com>
2022-07-20 19:09:53 +0200

    Add support for google::protobuf::Repeated(Ptr)Field in Collect()

--- test/collect.cc
+++ test/collect.cc
@@ -19,9 +19,9 @@ TEST(Collect, CommonVectorPass) {
 
   std::vector<int> result = *s();
 
+  ASSERT_EQ(2, result.size());
   EXPECT_EQ(5, result.at(0));
   EXPECT_EQ(12, result.at(1));
-  EXPECT_EQ(2, result.size());
 }
 
 TEST(Collect, CommonSetPass) {
@@ -37,6 +37,36 @@ TEST(Collect, CommonSetPass) {
   ASSERT_EQ(2, result.size());
   EXPECT_EQ(5, *result.begin());
   EXPECT_EQ(12, *++result.begin());
+}
+
+TEST(Collect, VectorToRepeatedPtrField) {
+  std::vector<std::string> v = {"Hello", "World"};
+
+  auto s = [&]() {
+    return Iterate(v)
+        | Collect<google::protobuf::RepeatedPtrField<std::string>>();
+  };
+
+  google::protobuf::RepeatedPtrField<std::string> result = *s();
+
+  ASSERT_EQ(2, result.size());
+  EXPECT_EQ("Hello", *result.begin());
+  EXPECT_EQ("World", *(result.begin() + 1));
+}
+
+TEST(Collect, VectorToRepeatedField) {
+  std::vector<int> v = {42, 25};
+
+  auto s = [&]() {
+    return Iterate(v)
+        | Collect<google::protobuf::RepeatedField<int>>();
+  };
+
+  google::protobuf::RepeatedField<int> result = *s();
+
+  ASSERT_EQ(2, result.size());
+  EXPECT_EQ(42, *result.begin());
+  EXPECT_EQ(25, *(result.begin() + 1));
 }
 
 } // namespace
