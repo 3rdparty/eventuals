ddc32fccfee1f0b0ed8d349aa8195c5949b800b2
Artur <arthur_bandaryk@mail.ru>
2022-07-22 21:00:18 +0300

    Remove unused files (#494)

--- test/failure-signal-handler.cc
+++
@@ -1,8 +0,0 @@
-#include "failure-signal-handler.h"
-
-// For setting up gtest environment in order to use
-// `glog` failure signal handler:
-// https://github.com/google/glog#failure-signal-handler
-static testing::Environment* const signal_handler_failure_env =
-    testing::AddGlobalTestEnvironment(
-        new InstallFailureSignalHandlerEnvironment{});
